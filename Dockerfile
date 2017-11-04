de711166fed8bf533f9438d8deffa94fe8618e82
todoroki <todoroki.godai@gmail.com>
2017-11-04 23:37:18 +0900

    infura version. but don't work

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,8 @@ FROM node:8.8.1-alpine
 COPY . /ico
 WORKDIR /ico
 
-RUN npm install -g truffle@3.4.8 ethereumjs-testrpc@4.1.3 && \
+RUN apk --no-cache add --virtual .deps git python make g++ tzdata && \
+    cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime && \
+    npm install -g truffle@3.4.8 ethereumjs-testrpc@4.1.3 && \
+    npm install && \
     yarn add zeppelin-solidity@1.3.0
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,9 @@
 const WakuWakuCoinCrowdsale = artifacts.require("./WakuWakuCoinCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
-  const endTime = startTime + (86400 * 20) // 20 days
-  const rate = new web3.BigNumber(1000)
-  const wallet = accounts[0]
-
-  deployer.deploy(WakuWakuCoinCrowdsale, startTime, endTime, rate, wallet)
+    const startTime = 1509804000 // 2017-11-04 23:00:00
+    const endTime = startTime + (60 * 10) // 10 minutes
+    const rate = 1000
+    const wallet = accounts[0]
+    deployer.deploy(WakuWakuCoinCrowdsale, startTime, endTime, rate, wallet)
 };
